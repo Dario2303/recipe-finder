4a30808cef1fe4532381907c7d9acf8f46c62686
dariorod <dario2303@gmail.com>
2023-07-26 18:33:21 -0300

    init project

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ function iniciarApp () {
     selectCategorias.addEventListener('change', seleccionarCategoria)
 
 
-obtenerCategorias();
+    obtenerCategorias();
 
     function obtenerCategorias() {
         const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
@@ -24,8 +24,34 @@ obtenerCategorias();
         })
     }
 
-    function seleccionarCategoria(select) {
-        console.log(select.target.value)
+    function seleccionarCategoria(e) {
+        const categoria = e.target.value;
+        const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`;
+        fetch(url)
+            .then(resp => resp.json())
+            .then(res => mostrarRecetas(res.meals))
+    }
+
+    function mostrarRecetas(recetas = []) {
+        //iterando en los resultados
+        recetas.forEach(receta => {
+
+            const { idMeal, strMeal, strMealThumb } = receta
+
+            const recetaContenedor = document.createElement('DIV');
+            recetaContenedor.classList.add('col-md-4')
+
+            const recetaCard = document.createElement('DIV')
+            recetaCard.classList.add('card', 'mb-4')
+
+            const recetaImagen = document.createElement('IMG')
+            recetaImagen.classList.add('card-img-top')
+            recetaImagen.alt = `Imagen de la receta ${strMeal}`
+            recetaImagen.src = strMealThumb;
+            
+            const recetaCardBody = document.createElement('DIV')
+            recetaCardBody.classList.add('card-body')
+        })
     }
 }
 
